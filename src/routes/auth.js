f0ba782c01514d872b9ef73b7cf9b32db1ab86b1
2547techno <technowizz2547@gmail.com>
2023-05-21 21:22:29 -0400

    write list to file

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = require("../../config.json")
+const fs = require('fs');
 
 router.get("/", async (req,res) => {
     if (req.query.error || !req.query.code) {
@@ -14,26 +15,26 @@ router.get("/", async (req,res) => {
         return res.redirect("/error")
     }
     
-    let userId;
+    let user
     try {
-        userId = await getUserId(token)
+        user = await getUser(token)
     } catch(err) {
         console.log(err);
         return res.redirect("/error")
     }
 
-    const bannedList = await getBannedList(token, userId)
+    const bannedList = await getBannedList(token, user.user_id)
 
-    let queryString = bannedList.map(u => u["user_login"]).join(",")
-
+    // let queryString = bannedList.map(u => u["user_login"]).join(",")
 
     const list = bannedList.map(u => u["user_login"])
     let listStr = "<pre>";
-    for(let user of list    ) {
-        listStr += `${user}<br>`
+    for(let login of list) {
+        listStr += `${login}<br>`
     }
     listStr += "</pre>"
 
+    saveList(list, user.login)
     res.send(listStr)
     // res.redirect(`/list?logins=${queryString}`)
 })
@@ -60,16 +61,20 @@ async function generateToken(code) {
     return token
 }
 
-async function getUserId(token) {
-    const idRes = await fetch("https://id.twitch.tv/oauth2/validate", {
+async function getUser(token) {
+    const userRes = await fetch("https://id.twitch.tv/oauth2/validate", {
         headers: {
             "Authorization": `OAuth ${token}`
         }
     }).then(res => res.json())
 
-    if (!idRes["user_id"]) throw new Error("No user_id!")
+    if (!userRes["user_id"]) throw new Error("No user_id!")
+    if (!userRes["login"]) throw new Error("No login!")
 
-    return idRes["user_id"]
+    return {
+        user_id: userRes["user_id"],
+        login: userRes["login"]
+    }
 }
 
 async function getBannedList(token, channel_id) {
@@ -106,6 +111,13 @@ async function requestBannedList(token, channel_id, cursor) {
     }).then(res => res.json())
 
     return bannedRes
+}
+
+function saveList(list, username) {
+    const now = new Date().getTime();
+
+    console.log("write");
+    fs.writeFileSync(`lists/${username}-${now}.txt`, list.join("\n"))
 }
 
 module.exports = router;
