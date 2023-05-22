94487322d5afeda17d2e32826ce45a1523d6e04d
2547techno <technowizz2547@gmail.com>
2023-05-21 20:25:09 -0400

    use clientid from config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const { REDIRECT_URI } = require("../../config.json")
+const { REDIRECT_URI, CLIENT_ID } = require("../../config.json")
 
 router.get("/", (req,res) => {
-    res.render("index", {REDIRECT_URI})
+    res.render("index", {REDIRECT_URI, CLIENT_ID})
 })
 
 module.exports = router;
